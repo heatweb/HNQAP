b2a6cf3ca2584e9a36d7ec2e94cf0a85410f8253
heatweb <admin@heatweb.com>
2024-06-24 20:57:12 +0100

    Update functions.sql

--- postgresql/functions.sql
+++ postgresql/functions.sql
@@ -395,21 +395,40 @@ BEGIN
 	runtime2 := runtime1 + intervalin;
 	
 	total_days_in_period := fn_total_days_in_period(time1,time2)::integer;
-	FOR cnt IN 1..total_days_in_period LOOP		
+	FOR cnt IN 1..total_days_in_period LOOP
+		
+	  	IF varkeyin = '*' THEN
 	
-		FOR avg_record IN
-			EXECUTE 'SELECT COUNT(time) AS value FROM '
-			|| quote_ident(networknode)
-			|| ' WHERE device = $1 AND vargroup = $2 AND varkey = $3 AND time>=$4 AND time<$5'
-			USING devicein, vargroupin, varkeyin, runtime1, runtime2
-		LOOP		
-
-			IF avg_record.value > 0 THEN
-				lcnt := lcnt + 1;
-			END IF;
+			FOR avg_record IN
+				EXECUTE 'SELECT COUNT(time) AS value FROM '
+				|| quote_ident(networknode)
+				|| ' WHERE device = $1 AND vargroup = $2 AND time>=$4 AND time<$5'
+				USING devicein, vargroupin, varkeyin, runtime1, runtime2
+			LOOP		
 	
-	    END LOOP;
+				IF avg_record.value > 0 THEN
+					lcnt := lcnt + 1;
+				END IF;
+		
+		    END LOOP;
 
+		ELSE
+
+			FOR avg_record IN
+				EXECUTE 'SELECT COUNT(time) AS value FROM '
+				|| quote_ident(networknode)
+				|| ' WHERE device = $1 AND vargroup = $2 AND varkey = $3 AND time>=$4 AND time<$5'
+				USING devicein, vargroupin, varkeyin, runtime1, runtime2
+			LOOP		
+	
+				IF avg_record.value > 0 THEN
+					lcnt := lcnt + 1;
+				END IF;
+		
+		    END LOOP;
+
+		END IF;
+		
 		runtime1 := runtime1 + intervalin;
 		runtime2 := runtime1 + intervalin;
 	
@@ -419,7 +438,6 @@ BEGIN
 	
 END;
 $$ LANGUAGE plpgsql;
-
 
 CREATE OR REPLACE FUNCTION fn_kpi_connectivity(networkin varchar, intervalin interval, time1 timestamp with time zone, time2 timestamp with time zone)
 RETURNS FLOAT AS $$
@@ -436,6 +454,29 @@ BEGIN
 	LOOP		
 		loopcnt := loopcnt + 1;
 		oot := oot + fn_kpi_connectivity(LOWER(avg_record.network||'_'||avg_record.node), avg_record.device, avg_record.vargroup, avg_record.varkey, intervalin, time1, time2);	
+	
+    END LOOP;
+	
+    RETURN (oot / loopcnt);
+    
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION fn_kpi_device_connectivity(networkin varchar, intervalin interval, time1 timestamp with time zone, time2 timestamp with time zone)
+RETURNS FLOAT AS $$
+DECLARE
+	avg_record RECORD;
+	oot FLOAT := 0;
+	loopcnt INTEGER := 0;
+BEGIN
+	FOR avg_record IN
+	   	EXECUTE 'SELECT t1.network,t1.node,t1.device,t1.vargroup,t1.varkey FROM readings t1'
+		|| ' INNER JOIN fields t2 ON t1.varkey = t2.varkey AND t1.vargroup = t2.vargroup'
+		|| ' WHERE t2.kpi = true AND network = $1' 
+   		USING networkin
+	LOOP		
+		loopcnt := loopcnt + 1;
+		oot := oot + fn_kpi_connectivity(LOWER(avg_record.network||'_'||avg_record.node), avg_record.device, avg_record.vargroup, '*', intervalin, time1, time2);	
 	
     END LOOP;
 	
